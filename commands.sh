36146beefa3445ef186f4a0199c90fbbf757dece
Bastián <bparedesp@falabella.cl>
2025-05-03 16:55:10 -0400

    siguiente

--- commands.sh
+++ commands.sh
@@ -1,2 +1,5 @@
-kubectl apply -f ./deployment.yaml
-kubectl port-forward service/experiments-service 3000:80
+kubectl apply -f ./deployments.yaml -f ./services.yaml
+kubectl port-forward service/back1-service 3000:80
+
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl port-forward svc/argocd-server -n argocd 8080:443
