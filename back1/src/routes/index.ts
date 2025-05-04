effc4707846448a5f7877bd735f39e7da6761fb4
Bastián <bparedesp@falabella.cl>
2025-05-04 11:32:42 -0400

    same namespace

--- back1/src/routes/index.ts
+++ back1/src/routes/index.ts
@@ -4,7 +4,7 @@ const routes = async (server: FastifyInstance) => {
   server.get('/', {}, async (_req, reply) => {
     console.log('ayuda desde back 1');
     const response = await fetch(
-      'http://back2-service.namespace2.svc.cluster.local'
+      'http://back2-service.default.svc.cluster.local'
     );
 
     const json = await response.json();
