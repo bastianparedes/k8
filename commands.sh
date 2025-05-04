3e0e89592e5de96a8a8b3ff48150681cc24325e2
Bastián <bparedesp@falabella.cl>
2025-05-03 22:55:31 -0400

    config

--- commands.sh
+++ commands.sh
@@ -3,6 +3,7 @@ docker build -t back2:latest -f ./back2/Dockerfile ./back2
 
 kubectl apply -f ./deployments.yaml -f ./services.yaml
 kubectl port-forward service/back1-service 3000:80 -n namespace1
+kubectl port-forward service/nginx-service 8081:80 -n namespace1
 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl port-forward svc/argocd-server -n argocd 8080:443
