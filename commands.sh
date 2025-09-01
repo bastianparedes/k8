af9a84e7dc0f3ab15638f73f23c88f3a403d47f7
Bastián <bparedesp@falabella.cl>
2025-09-01 01:03:43 -0400

    sdac

--- commands.sh
+++ commands.sh
@@ -16,7 +16,7 @@ kubectl port-forward service/nginx-service 8081:80 -n namespace1
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl port-forward svc/argocd-server -n argocd 8080:443
-kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 --decode
+kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 --decode # contraseña
 
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
